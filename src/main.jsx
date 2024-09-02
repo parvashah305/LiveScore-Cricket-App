2421f04b8ab4556245d5b017779841f93a983933
parvashah305 <parvashah305@gmail.com>
2024-09-02 21:40:40 +0530

    routes changed

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Live from '../Live.jsx'
+import Recent from '../Recent.jsx'
+import Upcoming from '../Upcoming.jsx'
+import DetailScorecard from "./Components/DetailScorecard"
 
 createRoot(document.getElementById('root')).render(
  <BrowserRouter>
-    <div>
+    {/* <div>
     <App />
-    </div>
+    </div> */}
+     <Routes>
+        <Route path="/" element={<Live/>}/>
+        <Route path="/recent" element={<Recent/>}/>
+        <Route path="/upcoming" element={<Upcoming/>}/>
+        <Route path="/scorecard/:id" element={<DetailScorecard/>} />
+      </Routes>
  </BrowserRouter>
 )
